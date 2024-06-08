7a2ea55279a7bd05398c342aa2e9abdfc67880e1
ericleonen <ericleonen@gmail.com>
2024-06-07 18:03:28 -0700

    added type property to Crease

--- src/origami/Crease.ts
+++ src/origami/Crease.ts
@@ -1,11 +1,14 @@
 import Vertex from "./Vertex";
 
+export type CreaseType = "M" | "V" | "N";
+
 /**
  * Represents a Crease between to Vertexes.
  */
 export default class Crease {
     vertex1: Vertex;
     vertex2: Vertex;
+    type: CreaseType;
 
     /**
      * Initializes a Crease object between two distinct Vertexes. If the given Vertexes are equal,
@@ -13,7 +16,7 @@ export default class Crease {
      * @param vertex1 
      * @param vertex2 
      */
-    constructor(vertex1: Vertex, vertex2: Vertex) {
+    constructor(vertex1: Vertex, vertex2: Vertex, type: CreaseType) {
         if (vertex1.equals(vertex2)) throw new Error("Creases need two distinct Vertexes.");
         else if (vertex1.compareTo(vertex2) > 0) {
             this.vertex2 = vertex1;
@@ -22,12 +25,14 @@ export default class Crease {
             this.vertex1 = vertex1;
             this.vertex2 = vertex2;
         }
+
+        this.type = type;
     }
 
     /**
      * Returns the string representation of this Crease: "[vertex1, vertex2]".
      */
     public toString(): string {
-        return `[${this.vertex1.toString()}, ${this.vertex2.toString()}]`;
+        return `${this.type}[${this.vertex1.toString()}, ${this.vertex2.toString()}]`;
     }
 }
