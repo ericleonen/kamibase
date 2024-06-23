a814c0ca554671c23e69b8ca4d6e10cb5866fb23
ericleonen <ericleonen@gmail.com>
2024-06-22 22:51:28 -0700

    fix comments

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,22 +1,26 @@
 import { PRECISION } from "@/settings";
 
 /**
- * Returns the canonically rounded version of x.
- * @param x A number
+ * Returns a rounded x to PRECISION decimal places.
  */
 export function round(x: number): number {
     return Number(x.toFixed(PRECISION));
 }
 
 /**
- * Returns true if numbers a and b are approximately equal, false otherwise.
- * @param a A number
- * @param b A number
+ * Returns whether or not a is approximately equal to b according to specified level of PRECISION.
  */
 export function approxEqual(a: number, b: number): boolean {
     return round(a - b) === 0;
 }
 
+/**
+ * Returns whether or not x is between min and max (inclusive). If min > max, throws an Error.
+ */
 export function inBetween(x: number, min: number, max: number): boolean {
+    if (min > max) {
+        throw new Error("min cannot be greater than max");
+    }
+
     return min <= x && x <= max;
 }
