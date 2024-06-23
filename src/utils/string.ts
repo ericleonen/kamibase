736d47a97ddeba7c1d10898188c42f2c488b08a7
ericleonen <ericleonen@gmail.com>
2024-06-22 22:54:19 -0700

    fix comments

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,18 +1,21 @@
 import { round } from "./math";
 
 /**
- * Capitalizes the first letter of a string and returns the string
- * @param str Word string to capitalize
+ * Capitalizes the first letter of a string and returns it. If str is empty, throws an Error.
  */
 export function capitalize(str: string) {
+    if (str.length === 0) {
+        throw new Error("str cannot be empty");
+    }
+
     return str.charAt(0).toUpperCase() + str.substring(1);
 }
 
 /**
- * Returns a list key of the items
- * @param items List of items
+ * Converts the given string or number arguments into a string key. Items are separated by spaces
+ * and numbers are rounded.
  */
-export function listKey(...items: any[]) {
+export function listKey(...items: (string | number)[]) {
     return items.map(item => {
         if (typeof item === "number") {
             return round(item);
