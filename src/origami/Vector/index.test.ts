f83442c98f52786855bc9fd2280cbd4cf6e60dcb
ericleonen <ericleonen@gmail.com>
2024-06-23 20:35:42 -0700

    fix tests

--- src/origami/Vector/index.test.ts
+++ src/origami/Vector/index.test.ts
@@ -1,32 +1,43 @@
 import Vector from ".";
 import { VERTEXES } from "../common";
 
+test("constructor() initializes a Vector", () => {
+    const vector = new Vector(0.25, 0.5);
+
+    // check x component
+    expect(vector.x).toBe(0.25);
+    // check y component
+    expect(vector.y).toBe(0.5);
+});
+
 test("Vector.fromVertexes() calculates a Vector from a head and a tail Vertex", () => {
     const tail = VERTEXES["center"];
     const head = VERTEXES["top right"];
     const vector = Vector.fromVertexes(tail, head);
 
+    // check x component
     expect(vector.x).toBe(0.5);
+    // check y component
     expect(vector.y).toBe(-0.5);
 });
 
 test("add() and subtract() add and subtract Vectors", () => {
-    let  vector = new Vector(0, 0);
-    vector = vector.add(new Vector(0.5, 0));
+    let vector = new Vector(0, 0);
 
+    // sum two vectors
+    vector = vector.add(new Vector(0.5, 0));
     expect(vector.x).toBe(0.5);
     expect(vector.y).toBe(0);
-
+    // subtract two vectors
     vector = vector.subtract(new Vector(1, 0.5));
-
     expect(vector.x).toBe(-0.5);
     expect(vector.y).toBe(-0.5);
 });
 
 test("scale() multiplies a Vector by a constant", () => {
     let vector = new Vector(0.5, -0.5);
-    vector = vector.scale(2);
 
+    vector = vector.scale(2);
     expect(vector.x).toBe(1);
     expect(vector.y).toBe(-1);
 });
@@ -34,23 +45,33 @@ test("scale() multiplies a Vector by a constant", () => {
 test("isParallelTo() confirms two Vectors are parallel", () => {
     const parallel1 = new Vector(1, 1.5);
     const parallel2 = new Vector(0.5, 0.75);
+    const notParallel = new Vector(-1, 1);
 
+    // vectors that are parallel
     expect(parallel1.isParallelTo(parallel2)).toBeTruthy();
+    // vectors that are not parallel
+    expect(parallel1.isParallelTo(notParallel)).toBeFalsy();
 });
 
 test("compareTo() compares two Vectors", () => {
     const smaller = new Vector(0, 0.5);
     const larger = new Vector(0, 1);
 
+    // smaller is less than larger
     expect(smaller.compareTo(larger)).toBeLessThan(0);
+    // larger is bigger than smaller
     expect(larger.compareTo(smaller)).toBeGreaterThan(0);
 });
 
 test("equals() confirms two Vectors are equal", () => {
     const equal1 = new Vector(1, 1);
     const equal2 = new Vector(1, 1);
+    const notEqual = new Vector(0.5, 0.5);
 
+    // vectors that are equal
     expect(equal1.equals(equal2)).toBeTruthy();
+    // vectors that are not equal
+    expect(equal1.equals(notEqual)).toBeFalsy();
 });
 
 test("dot() takes the dot product of two Vectors", () => {
--- src/origami/Vector/index.ts
+++ src/origami/Vector/index.ts
@@ -10,9 +10,7 @@ export default class Vector {
     readonly magnitude: number;
 
     /**
-     * Initializes a Vector object <x, y>
-     * @param x Horizontal number component
-     * @param y Vertical number component
+     * Initializes a Vector object <x, y>.
      */
     constructor(x: number, y: number) {
         this.x = x;
@@ -22,8 +20,6 @@ export default class Vector {
 
     /**
      * Returns Vector from the given tail Vertex to the head Vertex.
-     * @param tail Vector
-     * @param head Vector
      */
     public static fromVertexes(tail: Vertex, head: Vertex): Vector {
         return head.toVector().subtract(tail.toVector());
@@ -31,7 +27,6 @@ export default class Vector {
 
     /**
      * Returns the Vector sum of this and the other Vector.
-     * @param other Vector
      */
     public add(other: Vector) {
         return new Vector(this.x + other.x, this.y + other.y);
@@ -39,7 +34,6 @@ export default class Vector {
 
     /**
      * Returns the Vector difference between this and the other Vector.
-     * @param other Vector
      */
     public subtract(other: Vector) {
         return new Vector(this.x - other.x, this.y - other.y);
@@ -47,7 +41,6 @@ export default class Vector {
 
     /**
      * Returns this Vector scaled by the given constant.
-     * @param c A number
      */
     public scale(c: number) {
         return new Vector(this.x * c, this.y * c);
@@ -55,7 +48,6 @@ export default class Vector {
 
     /**
      * Returns true if this and the other Vector are parallel, false otherwise.
-     * @param other Vector
      */
     public isParallelTo(other: Vector) {
         return approxEqual(
@@ -67,7 +59,6 @@ export default class Vector {
     /**
      * Returns a positive number if this Vector is greater than the other Vector, a negative number
      * otherwise.
-     * @param other Vector
      */
     public compareTo(other: Vector): number {
         return round(this.x - other.x) || round(this.y - other.y);
@@ -75,7 +66,6 @@ export default class Vector {
 
     /**
      * Returns true if this and the other Vector are equal, false otherwise.
-     * @param other Vector
      */
     public equals(other: Vector): boolean {
         return this.compareTo(other) === 0;
@@ -83,7 +73,6 @@ export default class Vector {
 
     /**
      * Returns the dot of this and the other Vector.
-     * @param other Vector
      */
     public dot(other: Vector): number {
         return this.x * other.x + this.y * other.y;
