06a888f6143a62650d37d92601cce73a632c3f6e
ericleonen <ericleonen@gmail.com>
2024-07-27 12:58:40 -0700

    rename from App to Database

--- src/app/(database)/page.tsx
+++ src/app/(database)/page.tsx
@@ -3,7 +3,7 @@
 import KamiCards from "./components/KamiCards";
 import TopBar from "./components/TopBar";
 
-export default function App() {
+export default function Database() {
 	return (
 		<div className="h-screen items-center flex-col bg-theme-white overflow-y-scroll">
 			<TopBar />
